92678ae947cac3b4f8178d79ac4f6adf7a127d4e
Siddhant Bane <thatao.sid@gmail.com>
2019-12-31 10:55:08 +0530

    Create low_t.ino

---
+++ Arduino_files/low_t/low_t.ino
@@ -0,0 +1,70 @@
+int buz=12;
+int green=9;
+int yellow=10;
+int red=11;
+int num_array[10][7] = {  { 1,1,1,1,1,1,0 },    
+                          { 0,1,1,0,0,0,0 },   
+                          { 1,1,0,1,1,0,1 },    
+                          { 1,1,1,1,0,0,1 },    
+                          { 0,1,1,0,0,1,1 },    
+                          { 1,0,1,1,0,1,1 },    
+                          { 1,0,1,1,1,1,1 },    
+                          { 1,1,1,0,0,0,0 },    
+                          { 1,1,1,1,1,1,1 },    
+                          { 1,1,1,1,0,1,1 }};
+
+
+void setup() 
+{
+ Serial.begin(9600);
+ Serial.println("<ARDUINO IS READY>");
+ pinMode(2, OUTPUT);
+ pinMode(3, OUTPUT);
+ pinMode(4, OUTPUT);
+ pinMode(5, OUTPUT);
+ pinMode(6, OUTPUT);
+ pinMode(7, OUTPUT);
+ pinMode(8, OUTPUT);
+ pinMode(9, OUTPUT);
+ pinMode(10, OUTPUT);
+ pinMode(11, OUTPUT);
+ pinMode(12,OUTPUT);
+ }
+void loop() 
+ {
+// digitalWrite(12,LOW);
+  for (int counter = 10; counter > 0; --counter)
+  {
+    if(counter==10)
+    {
+ Serial.println("<READY TO RECEIVE SIGNAL>");Serial.println("<RECIEVED SIGNAL '0':TRAFFIC LOW>");
+    }
+    delay(1000);
+   if (counter==10||counter==9||counter==8||counter==7||counter==6)
+   {
+    digitalWrite(11,HIGH);digitalWrite(9,LOW); Num_Write(counter-1);Serial.println("<RED LIGHT IS ON>");
+    }
+   else if(counter==5||counter==4||counter==3||counter==2) 
+   {
+    digitalWrite(10,HIGH);digitalWrite(11,LOW); Num_Write(counter-1);Serial.println("<YELLOW LIGHT IS ON>");
+    }
+   else
+   {
+    digitalWrite(9,HIGH);digitalWrite(10,LOW); Num_Write(counter-1);Serial.println("<GREEN LIGHT IS ON FOR 20 SECONDS>");delay(20000);
+    }
+ 
+  // Num_Write(counter-1);
+   }
+//digitalWrite(12,HIGH);
+ // delay(1000);
+} 
+void Num_Write(int number)
+ {
+  int pin= 2;
+   for (int j=0; j < 7; j++)
+ {
+   digitalWrite(pin, num_array[number][j]);
+    pin++;
+   }
+ }
+
